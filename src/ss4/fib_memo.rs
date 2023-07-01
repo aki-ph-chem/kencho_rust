5837b2c6ca2a45b9a23cfdeea4cbcc838563ef62
aki <aki@mymail.com>
2023-07-01 10:33:31 +0900

    メモ化したフィボナッチ数列

---
+++ src/ss4/fib_memo.rs
@@ -0,0 +1,38 @@
+fn fib_rec(n: i64,  array: &mut Vec<i64>) -> i64 {
+    let index = n as usize;
+    match index {
+        0 => 1,
+        1 => 1,
+        _ => {
+            if array[index] != -1 {
+                array[index]
+            } else {
+                array[index] = fib_rec(n - 1, array) + fib_rec(n - 2, array);
+                array[index]
+            }
+        }
+    } 
+}
+
+fn fib_dp(n: i64, array: &mut Vec<i64>){
+    array[0] = 1; array[1] = 1;
+    for i in 2..(n as usize + 1) {
+        array[i] = array[i - 1] + array[i - 2];
+    } 
+}
+
+fn main() {
+    const ARRAY_SIZE:usize = 50;
+
+    let mut array:Vec<i64> = vec![-1;ARRAY_SIZE];
+    println!("fib_rec(49, &mut array) = {}", fib_rec(49, &mut array));
+    for (index, value) in array.iter().enumerate()  {
+                println!("fib_{} = {}", index, value);
+    }
+    
+    let mut array_2:Vec<i64> = vec![0; ARRAY_SIZE];
+    fib_dp(49, &mut array_2);
+    for (index, value) in array_2.iter().enumerate()  {
+                println!("fib_{} = {}", index, value);
+    }
+}
