fec972ed685fffd46637a6286a281b1cdc0f0f13
aki <aki@mymail.com>
2023-08-08 12:40:18 +0900

    ヒープの実装:ok

--- src/ss10/heap.rs
+++ src/ss10/heap.rs
@@ -1,5 +1,3 @@
-use proconio::input;
-
 struct Heap {
     heap: Vec<i32>,
 }
@@ -28,13 +26,61 @@ impl Heap {
         self.heap[i] = x;
     }
 
+    // 最大値を知る
     fn top(&self) -> i32 {
-        1
+        if !self.heap.is_empty() {
+            self.heap[0]
+        } else {
+            -1
+        }
     }
 
+    // 最大値を削除
     fn pop(&mut self) {
+        if self.heap.is_empty() {
+            return;
+        }
+        // 頂点に持ってくる値
+        let x = self.heap.iter()
+            .rev()
+            .next()
+            .unwrap()
+            .clone();
+        self.heap.pop();
+        // 根から下ろしていく
+        let mut i = 0;
+        while i * 2 + 1 < self.heap.len() {
+            // 子の値を比較して大きい方をchild_1とする
+            let mut child_1 = i * 2 + 1;
+            let child_2 = i * 2 + 2;
+            if child_2 < self.heap.len() 
+                && self.heap[child_2] > self.heap[child_1] {
+                    child_1 = child_2;
+            }
+            // 逆転がなければ終了
+            if self.heap[child_1] <= x {
+                break;
+            }
+            // 自分の値を子の値にする
+            self.heap[i] = self.heap[child_1];
+            // 自分は下に行く
+            i = child_1;
+        }
+        // xを最終的にここにもっていく
+        self.heap[i] = x;
     }
 }
 
 fn main() {
+    let mut h = Heap::new();
+    h.push(5); h.push(3); h.push(7); h.push(1);
+
+    println!("h.top() = {}", h.top()); // 7
+    h.pop();
+
+    println!("h.top() = {}", h.top()); // 5 
+    h.pop();
+
+    println!("h.top() = {}", h.top()); // 11 
+    h.pop();
 }
