aa37572f91f78fe6807c514fec7a06e74d0dfb2b
aki <aki@mymail.com>
2023-08-22 02:30:12 +0900

    とりあえず書けた:正当性はまだ未検証

--- src/ss13/dp_tree.rs
+++ src/ss13/dp_tree.rs
@@ -3,7 +3,11 @@ use proconio::input;
 
 type Graph = Vec<Vec<usize>>;
 
-fn dfs(graph: &Graph, v: usize, p: usize) {
+// 木の上での探索の基本形
+// v: 現在探索中の頂点
+// p: vの親(vが根であるときはp = -1)
+fn dfs(graph: &Graph, subtree_size: &mut Vec<usize>, depth: &mut Vec<usize>, v: usize, p: usize, d: usize) {
+    depth[v] = d;
     for c in &graph[v] {
         // 探索が親方向へ逆流するのを防ぐ
         if *c == p {
@@ -11,7 +15,17 @@ fn dfs(graph: &Graph, v: usize, p: usize) {
         }
 
         // cはvの各子頂点を動く
-        dfs(graph, *c, v);
+        dfs(graph, subtree_size, depth, *c, v, d + 1);
+    }
+
+    // 帰りがけに部分木のサイズを求める
+    subtree_size[v] = 1;
+    for c in &graph[v] {
+        if *c == p {
+            continue;
+        }
+        // 小頂点を寝とする部分木のサイズを加算する
+        subtree_size[v] += subtree_size[*c];
     }
 }
 
@@ -23,11 +37,16 @@ fn main() {
         pair: [(usize,usize); n - 1],
     }
 
-    // 有向グラフ
     let mut graph: Graph= vec![vec![]; n];
     for (a, b) in pair {
         graph[a].push(b);
+        graph[b].push(a);
     }
     println!("graph = {:#?}", graph);
+    let mut subtree_size = vec![0; n];
+    let mut depth = vec![0; n];
+    dfs(&graph, &mut subtree_size, &mut depth, 0, std::usize::MAX, 0);
 
+    println!("depth = {:#?}", depth);
+    println!("subtree_size = {:#?}", subtree_size);
 }
