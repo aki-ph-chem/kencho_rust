dc8f9eef4dca39341ba5c7503192defb10f2f465
aki <aki@mymail.com>
2023-08-28 20:00:36 +0900

    ヒープを使ったダイクストラ:ok

--- src/ss14/dijkstra_heap.rs
+++ src/ss14/dijkstra_heap.rs
@@ -1,5 +1,5 @@
 use proconio::input;
-use std::collections::binary_heap;
+use std::collections::BinaryHeap;
 
 const INF:i64 = 1 << 60;
 
@@ -32,6 +32,7 @@ where T: std::cmp::PartialOrd
 }
 
 // (dist[v], v)を保持
+#[derive(Eq, Ord, PartialEq, PartialOrd)]
 struct Piar(i64, usize);
 
 fn main() {
@@ -54,4 +55,32 @@ fn main() {
     // ダイクストラ法
     let mut dist = vec![INF; n];
     dist[s] = 0;
+    let mut que = BinaryHeap::new();
+    que.push(Piar(dist[s], s));
+    // ダイクストラ法の反復
+    while !que.is_empty() {
+        //v: 使用済みでない頂点のうちd[v]お最小の頂点
+        let v = que.peek().unwrap().1;
+        let d = que.peek().unwrap().0;
+        que.pop();
+        // dがゴミ化否か判定
+        if d > dist[v] {
+            continue;
+        }
+        for e in &graph_weight[v] {
+            // 更新があればヒープに新たに挿入
+            let tmp = dist[v] + e.weight;
+            if chmin(&mut dist[e.to], tmp) {
+                que.push(Piar(dist[e.to], e.to));
+            }
+        }
+    }
+    // 結果の出力
+    for v in 0..n {
+        if dist[v] < INF {
+            println!("{}", dist[v]);
+        } else {
+            println!("INF");
+        }
+    }
 }
