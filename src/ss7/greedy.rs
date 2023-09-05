f75eb05e9cd2cbc781f517c3296c9db36ebe4f6c
aki <aki@mymail.com>
2023-09-05 10:30:32 +0900

    fix

--- src/ss7/greedy.rs
+++ src/ss7/greedy.rs
@@ -2,21 +2,20 @@ use proconio::input;
 
 fn main() {
     input!{
-        a: [i32;6],
         mut value: i32,
+        a: [i32;6],
     }
     let coins = vec![500, 100, 50, 10, 5, 1];
 
     let mut res = 0;
     for i in 0 .. 6 {
-        let q = value / coins[i];
-        if q < a[i] {
-            res += q;
-            value -= q * coins[i];
-        } else {
-            res += a[i];
-            value -= a[i] * coins[i];
+        let mut q = value / coins[i];
+        if q > a[i] {
+            q = a[i];
         }
+
+        value -= coins[i] * q; 
+        res += q;
     }
     println!("{}", res);
 }
