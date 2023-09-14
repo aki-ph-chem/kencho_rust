0ded96ce728c9f68ba5b174a6fb060b39891427c
aki <aki@mymail.com>
2023-09-14 23:24:49 +0900

    fib_0 = 0と修正

--- src/ss4/fib_for.rs
+++ src/ss4/fib_for.rs
@@ -1,15 +1,15 @@
 fn fib(n: i64) -> i64 {
     match n {
-        0 => 1,
+        0 => 0,
         1 => 1,
         _ => {
             let mut fib = 0;
+            let mut fib_0 = 0; 
             let mut fib_1 = 1; 
-            let mut fib_2 = 1; 
             for _i in 2 .. n + 1 {
-                fib = fib_1 + fib_2;
-                fib_1 = fib_2;
-                fib_2 = fib;
+                fib = fib_0 + fib_1;
+                fib_0 = fib_1;
+                fib_1 = fib;
             }
             fib
         },
