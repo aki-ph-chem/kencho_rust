61af957586441beed8fde0fd3d56ad78798403aa
aki <aki@mymail.com>
2023-09-14 23:31:03 +0900

    関数の引数の型、fib_0 を修正した

--- src/ss4/fib_memo.rs
+++ src/ss4/fib_memo.rs
@@ -1,22 +1,21 @@
-fn fib_rec(n: i64,  array: &mut Vec<i64>) -> i64 {
-    let index = n as usize;
+fn fib_rec(index: usize,  array: &mut Vec<i64>) -> i64 {
     match index {
-        0 => 1,
+        0 => 0,
         1 => 1,
         _ => {
             if array[index] != -1 {
                 array[index]
             } else {
-                array[index] = fib_rec(n - 1, array) + fib_rec(n - 2, array);
+                array[index] = fib_rec(index - 1, array) + fib_rec(index - 2, array);
                 array[index]
             }
         }
     } 
 }
 
-fn fib_dp(n: i64, array: &mut Vec<i64>){
-    array[0] = 1; array[1] = 1;
-    for i in 2..(n as usize + 1) {
+fn fib_dp(n: usize, array: &mut Vec<i64>){
+    array[0] = 0; array[1] = 1;
+    for i in 2..(n + 1) {
         array[i] = array[i - 1] + array[i - 2];
     } 
 }
@@ -26,8 +25,8 @@ fn main() {
 
     let mut array:Vec<i64> = vec![-1;ARRAY_SIZE];
     println!("fib_rec(49, &mut array) = {}", fib_rec(49, &mut array));
-    for (index, value) in array.iter().enumerate()  {
-                println!("fib_{} = {}", index, value);
+    for (index, value) in array.iter().skip(2).enumerate()  {
+                println!("fib_{} = {}", index + 2, value);
     }
     
     let mut array_2:Vec<i64> = vec![0; ARRAY_SIZE];
